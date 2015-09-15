010e84de79d9e164b0256b2d507bef03c11844f4
Artem Slabodnik <artem.slabodnik@gmail.com>
2015-09-15 14:37:15 +0300

    wrote actions for creating some info about stock inwards

--- app/controllers/admin/stock_inwards_controller.rb
+++ app/controllers/admin/stock_inwards_controller.rb
@@ -1,2 +1,46 @@
 class Admin::StockInwardsController < ApplicationController
+  respond_to :json
+
+  def index
+    @stock_inwards = StockInward.all
+
+    render :json => @stock_inwards
+  end
+
+  def show
+    @stock_inward = StockInward.find(params[:id])
+
+    render :json => @stock_inward
+  end
+
+  def new
+    @stock_inward = StockInward.new
+  end
+
+  def create
+    @stock_inward = StockInward.new(params[:stock_inward])
+    @stock_inward.save
+
+    render :json => @stock_inward
+  end
+
+  def edit
+    @stock_inward = StockInward.find(params[:id])
+
+    render :json => @stock_inward
+  end
+
+  def update
+    @stock_inward = StockInward.find(params[:id])
+    @stock_inward.update_attributes(params[:stock_inward])
+
+    render :json => @stock_inward
+  end
+
+  def destroy
+    @stock_inward = StockInward.find(params[:id])
+    @stock_inward.destroy
+
+    render :json => @stock_inward
+  end
 end
