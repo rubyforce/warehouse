38f9f2b15957dbea3a69a142a43d3709d8849e9e
Artem Slabodnik <artem.slabodnik@gmail.com>
2015-09-23 12:02:23 +0300

    added new field for items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923083533) do
+ActiveRecord::Schema.define(version: 20150923085920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -285,6 +285,7 @@ ActiveRecord::Schema.define(version: 20150923083533) do
     t.integer  "container_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.decimal  "tax"
   end
 
   create_table "ledgers", force: :cascade do |t|
