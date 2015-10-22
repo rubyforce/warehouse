e232d509ca3565a53b8b3e7eec9dbbcff9a7b766
Artem Slabodnik <artem.slabodnik@gmail.com>
2015-10-22 23:24:35 +0300

    removed repeating migrations

--- db/migrate/20151016121551_add_fields_to_vehicle_loadings.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToVehicleLoadings < ActiveRecord::Migration
-  def change
-    add_column :vehicle_loadings, :vehicle_id, :integer
-    add_column :vehicle_loadings, :warehouse_id, :integer
-    add_column :vehicle_loadings, :device_id, :integer
-  end
-end
