d54e9a34b96a7823942ec6476e9030024eace36a
Artem Slabodnik <artem.slabodnik@gmail.com>
2015-10-23 16:29:59 +0300

    belongs to our payment voucehr model

--- app/models/payment_voucher.rb
+++ app/models/payment_voucher.rb
@@ -1,5 +1,5 @@
 class PaymentVoucher < ActiveRecord::Base
-  belongs_to :ledger, class_name: "Ledger", foreign_key: "ledger_id"
+  belongs_to :vendor, class_name: "Vendor", foreign_key: "vendor_id"
   belongs_to :employee, class_name: "Employee", foreign_key: "employee_id"
   belongs_to :expense, class_name: "Expense", foreign_key: "expense_id"
 end
